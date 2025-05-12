f9cf21d815e0a1eada36ead3ecdef67843430a1b
OscarKaluza <Oscarkaluza1@gmail.com>
2025-05-12 15:59:20 +0200

    -

--- SyncService.Data/obj/Debug/net8.0/SyncService.Data.AssemblyInfo.cs
+++ SyncService.Data/obj/Debug/net8.0/SyncService.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyncService.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec1f038272cbed771007440a5bf95662fb24b41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646738671d0503c6234521bc52022b2daeb7fe90")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyncService.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyncService.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
